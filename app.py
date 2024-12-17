c3ce308e992f02e5c3f31b8f132fb273f8e8ca06
bruno <105159359+brfury@users.noreply.github.com>
2024-12-17 12:17:13 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,109 @@
+from flask import Flask, request, jsonify
+import chromadb
+from chromadb.config import Settings
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+from data.pipeline import TextProcessor
+
+class TextSimilarityAPI:
+    def __init__(self, db_path, embedding_model_path):
+        # Configurações do ChromaDB
+        self.chroma_settings = Settings(persist_directory=db_path, anonymized_telemetry=False)
+
+        # Inicialização do cliente do ChromaDB e modelo de embeddings
+        self.embedding_model =  SentenceTransformer(embedding_model_path)
+        self.client = chromadb.PersistentClient(path=self.chroma_settings.persist_directory, settings=self.chroma_settings)
+        self.collection = self.client.get_or_create_collection(name="text_similarity", metadata={"hnsw:space": "cosine"})
+
+        # Inicialização do processador de texto
+        self.text_processor = TextProcessor()
+
+    def add_text(self, text_id, text):
+        text_clean = self.text_processor.preprocess_text(text)
+        embedding = self.embedding_model.encode([text_clean])[0]
+        
+        self.collection.add(
+            documents=[text],
+            ids=[text_id],
+            embeddings=[embedding.tolist()]
+        )
+        return text_clean
+
+    def search_text(self, query, top_k):
+        query_embedding = self.embedding_model.encode([query])[0]
+        results = self.collection.query(
+            query_embeddings=[query_embedding.tolist()],
+            n_results=top_k
+        )
+        return results
+
+    def bulk_add_texts(self, csv_path):
+        data = pd.read_csv(csv_path)
+        unique_texts = data['Text2'].unique()
+
+        for idx, text in enumerate(unique_texts):
+            text_clean = text #self.text_processor.preprocess_text(text)
+            embedding = self.embedding_model.encode([text])[0]
+
+            self.collection.add(
+                documents=[text],
+                ids=[str(idx)],
+                embeddings=[embedding.tolist()]
+            )
+
+
+
+
+text_similarity_api = TextSimilarityAPI(db_path="db_dir", embedding_model_path=r"models\temtembinary")
+text_similarity_api.bulk_add_texts(r"dataset_binario_novos_nomes.csv")
+app = Flask(__name__)
+@app.route('/add_text', methods=['POST'])
+def api_add_text():
+    try:
+        data = request.get_json()
+        text_id = data['id']
+        text = data['text']
+
+        text_clean = text_similarity_api.add_text(text_id, text)
+        return jsonify({"message": f"Texto '{text_clean}' com ID '{text_id}' adicionado com sucesso."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/search_text', methods=['POST'])
+def api_search_text():
+    try:
+        data = request.get_json()
+        query = data['query']
+        top_k = data.get('top_k', 10)
+
+        results = text_similarity_api.search_text(query, top_k)
+
+        if results["documents"]:
+            top_results = [
+                {
+                    "document": results["documents"][0][i],
+                    "id": results["ids"][0][i],
+                    "similarity": 1 - results["distances"][0][i]
+                }
+                for i in range(len(results["documents"][0]))
+            ]
+
+            return jsonify({
+                "total_results": len(top_results),
+                "top_results": top_results
+            }), 200
+        else:
+            return jsonify({"message": "Nenhum texto encontrado similar."}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/bulk_add_texts', methods=['POST'])
+def api_bulk_add_texts():
+    try:
+        text_similarity_api.bulk_add_texts(csv_path='base_de_dados_corrigida.csv')
+        return jsonify({"message": "Textos adicionados com sucesso."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
