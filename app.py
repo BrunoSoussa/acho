96870a2f3357143cf0ba67515512848d82918650
bruno <105159359+brfury@users.noreply.github.com>
2024-12-27 15:27:45 -0300

    top k=10

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def api_search_text():
     try:
         data = request.get_json()
         query = data['query']
-        top_k = data.get('top_k', 15)
+        top_k = data.get('top_k', 10)
 
         results = text_similarity_api.search_text(query, top_k)
 
